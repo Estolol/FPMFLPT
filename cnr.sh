5b098278bd3432105860c66720f4bc644136cfb8
lshao <laetitia.shao@gmail.com>
2017-02-11 15:13:59 +0100

    Changed gitignore

--- cnr.sh
+++ cnr.sh
@@ -5,9 +5,9 @@ if [ "$#" -ne 1 ] || ! [ -f "input/$1.txt" ]; then
   exit 1
 fi
 
-JNA_PATH="/usr/share/JNA/jna.jar"
-PARALLEL_PATH="/usr/share/parallel-colt/parallelcolt.jar"
-JOM_PATH="/usr/share/JOM/jom.jar"
+JNA_PATH="/usr/share/jna/jna.jar"
+PARALLEL_PATH="/usr/share/parallelcolt/parallelcolt-0.9.4.jar"
+JOM_PATH="/usr/share/jom/jom-0.2.0.1.jar"
 #GLPK_PATH="/usr/share/java/glpk-java.jar"
 #sed -i 's:String input =:String input = args[0];//:g' src/main.java
 javac -cp "$JOM_PATH:$JNA_PATH:$PARALLEL_PATH" -d bin src/*.java
